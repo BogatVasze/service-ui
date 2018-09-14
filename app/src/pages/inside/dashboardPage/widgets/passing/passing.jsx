0ae71f5e3773c1d824baddee81aa7eaed13eafca
Attila Bognar1 <Attila_Bognar1@epam.com>
2018-09-14 13:36:39 +0200

    Add chart rendering and refactor component

--- app/src/pages/inside/dashboardPage/widgets/passing/passing.jsx
+++ app/src/pages/inside/dashboardPage/widgets/passing/passing.jsx
@@ -1,25 +1,93 @@
 import { Component } from 'react';
-import { oneOf } from 'prop-types';
-import {
-  STATS_FAILED,
-  STATS_SKIPPED,
-  STATS_PASSED,
-  STATS_TOTAL,
-} from 'common/constants/statistics';
+import { PropTypes } from 'prop-types';
+import classNames from 'classnames/bind';
 
-const PTIssueType = oneOf([STATS_FAILED, STATS_SKIPPED, STATS_PASSED, STATS_TOTAL]);
+import C3Chart from 'react-c3js';
+import styles from './passing.scss';
+import { LaunchInfoBlock } from '../components/testsTableWidget/launchInfoBlock';
+import { ChartLegend } from '../components/chartLegend';
+import { Labels } from '../components/labels';
+
+const cx = classNames.bind(styles);
+const legends = {
+  colors: ['#8db677', '#ed6c49'],
+  labels: ['Passing', 'Failed'],
+};
 
 class Passing extends Component {
   static propTypes = {
-    issueType: PTIssueType.isRequired,
+    launch: PropTypes.array.isRequired,
+    data: PropTypes.array.isRequired,
   };
 
+  constructor() {
+    super();
+    this.chartData = {
+      columns: [],
+    };
+    this.passedPercentage = 0;
+    this.failedPercentage = 0;
+  }
   state = {
     chartType: 'bar',
   };
 
+  componentWillMount() {
+    this.chartData = this.getChartData();
+  }
+
+  getPercentage = (a, b) => Math.round(a / b * 100);
+
+  getProcessedData = (data) => {
+    this.passedPercentage = this.getPercentage(data[0].values.passed, data[0].values.total);
+    this.failedPercentage = this.getPercentage(
+      data[0].values.total - data[0].values.passed,
+      data[0].values.total,
+    );
+
+    return [[legends.labels[0], this.passedPercentage], [legends.labels[1], this.failedPercentage]];
+  };
+
+  getChartData = () => ({
+    columns: this.getProcessedData(this.props.data),
+    type: this.state.chartType,
+    groups: [[legends.labels[0], legends.labels[1]]],
+    colors: {
+      Passing: legends.colors[0],
+      Failed: legends.colors[1],
+    },
+    labels: this.state.chartType !== 'bar',
+    order: null,
+  });
+
   render() {
-    return <div style={{ backgroundColor: 'green', width: '400px', height: '200px' }} />;
+    return (
+      <div className={cx('passing')}>
+        <header>
+          <ChartLegend
+            legends={legends}
+            alignment={this.state.chartType === 'bar' ? 'horizontal' : 'vertical'}
+          />
+          <LaunchInfoBlock launchName={this.props.launch[0]} />
+        </header>
+        <section>
+          {this.state.chartType === 'bar' ? (
+            <Labels
+              labels={[this.passedPercentage, this.failedPercentage]}
+              widths={[this.passedPercentage, this.failedPercentage]}
+            />
+          ) : null}
+          <C3Chart
+            data={this.chartData}
+            bar={{ width: 50 }}
+            axis={{ rotated: true, x: { show: false, padding: 0 }, y: { show: false, padding: 0 } }}
+            legend={{ show: false }}
+            size={{ height: 200 }}
+            tooltip={{ show: false }}
+          />
+        </section>
+      </div>
+    );
   }
 }
 
--- app/src/pages/inside/dashboardPage/widgets/passing/passing.stories.jsx
+++ app/src/pages/inside/dashboardPage/widgets/passing/passing.stories.jsx
@@ -1,7 +1,22 @@
 import { storiesOf } from '@storybook/react';
+import { host } from 'storybook-host';
 import Passing from './passing';
 import { passing } from './data';
 
-storiesOf('Pages/inside/dashboardPage/passing', module).add('default state', () => (
-  <Passing issueType={passing.issueType} />
-));
+storiesOf('Pages/inside/dashboardPage/passing', module)
+  .addDecorator(
+    host({
+      title: 'Passing rate per launch',
+      align: 'center middle',
+      backdrop: 'rgba(70, 69, 71, 0.2)',
+      background: '#fff',
+      height: 300,
+      width: '100%',
+    }),
+  )
+  .add('default state', () => (
+    <Passing
+      launch={passing.content_parameters.widgetOptions.launchNameFilter}
+      data={passing.content.result}
+    />
+  ));
